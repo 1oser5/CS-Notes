2da56aa4894ff08ad9c485c4655922bce24ef615
1oser5 <snoopy98@163.com>
2020-01-02 17:55:01 +0800

    property、slots 以及 with

--- 书籍/Python3-cookbook/chapter8.py
+++ 书籍/Python3-cookbook/chapter8.py
@@ -55,3 +55,118 @@ format(d) # '2012-12-21'
 
 # 3.让对象支持上下文管理协议
 """ 定义 __enter__ 和 __exit__ 实现 with 语句 """
+# 实现一个简单的网络连接
+from socket import socket, AF_INET, SOCK_STREAM
+class LazyConnection(object):
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = family
+        self.type = type
+        self.sock = None
+    def __enter__(self):
+        if self.sock is not None:
+            raise RuntimeError('Already connected!')
+        self.sock = socket(self.family, self.type)
+        self.sock.connect(self.address)
+        return self.sock
+    def __exit__(self, exc_tu, exc_val, tb):
+        self.sock.close()
+        self.sock = None
+# Usage，属于懒连接，在定义时什么都不做
+conn = LazyConnection(('www.python.org', 80))
+with conn as s:
+    pass
+
+# 4.创建大量对象的内存优化
+""" 你可以使用 __slots__ 属性来极大地减少实例所占的内存
+ATTENTION 当你这样做时，Python 会为实例使用一种更紧凑的内部表示
+实例通过一个固定大小的数组来构建，而不是每个实例定义一个字典，和元组和列表类似
+但我们使用了 __slots__ 之后无法定义新的属性，只能使用那些属性
+将 __slots__ 作为封装工具来防止用户增加新的属性，并不是其初衷，请将它作为一个内存管理工具来使用
+"""
+class Date_(object):
+    __slots__ = ['year', 'month', 'day']
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+# 5.在类中封装属性名
+""" Python 没有私有变量的限制，但是程序员遵循一定的属性和方法命名来达到这个效果
+第一个约定: 任何以单下划线 _ 开头的名字都应该是内部实现
+第二个约定: 使用双下划线会导致访问名称会变成其他形式，使得其在继承中无法被覆盖
+第三个约定: 如果你的变量命名和保留字冲突，在后面加上 _
+"""
+# 单个下划线是内部实现
+class A(object):
+    def __init__(self):
+        self._internal = 0
+# 双下划线其无法被覆盖
+class B(object):
+    def __init__(self):
+        self.__private = 0
+    def __private_method(self):
+        pass
+class C(B):
+    def __init__(self):
+        super().__init__()
+        self.__private = 1 # Does not override B.__private
+    # Does not override B.__private_method()
+    def __private_method(self):
+        pass
+
+# 6.创建可管理的属性
+""" 使用 property 进行类管理
+可以将已有的方法定义为 property
+定义为 property 的属性是懒加载的
+可以用其让代码变得优雅，而不是臃肿
+"""
+# 使用 property 进行类检查
+class Person(object):
+    def __init__(self, first_name):
+        self.first_name = first_name
+    @property
+    def first_name(self):
+        return self._first_name
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Excepted a string')
+        self._first_name = value
+# 将已有方法定义为 property
+class P(object):
+    def __init__(self, first_name):
+        self.set_first_name(first_name)
+    # Getter function
+    def get_first_name(self):
+        return self._first_name
+    # Setter function
+    def set_first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string')
+        self._first_name = value
+    # Deleter function (optional)
+    def del_first_name(self):
+        raise AttributeError("Can't delete attribute")
+    # Make a property from existing get/set methods
+    name = property(get_first_name, set_first_name, del_first_name)
+# 懒加载，优雅的访问属性
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @property
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+c = Circle(4.0)
+c.radius # 4.0
+c.area # 50.26548245743669
